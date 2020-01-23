01c178e8407dc00973ca6da4373d889d98355dfe
Alper Battal <alprbttl@gmail.com>
2020-01-23 11:38:54 -0500

    git ignore file added

--- src/test/java/com/automation/tests/Day2/ORDSTests.java
+++ src/test/java/com/automation/tests/Day2/ORDSTests.java
@@ -1,4 +1,13 @@
 package com.automation.tests.Day2;
 
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static
+
 public class ORDSTests {
+
+    @Test
+    public void test(){
+
+    }
 }
