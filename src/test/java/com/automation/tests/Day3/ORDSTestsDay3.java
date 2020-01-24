a1dec6166a993c290d0ffeb6cc1dac4da527922e
Alper Battal <alprbttl@gmail.com>
2020-01-24 17:04:26 -0500

    List interface for salaries

--- src/test/java/com/automation/tests/Day3/ORDSTestsDay3.java
+++ src/test/java/com/automation/tests/Day3/ORDSTestsDay3.java
@@ -2,6 +2,7 @@ package com.automation.tests.Day3;
 
 import com.automation.utilities.ConfigurationReader;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.BeforeAll;
 import io.restassured.http.Header;
@@ -9,6 +10,9 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Time;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.*;
@@ -158,11 +162,92 @@ public class ORDSTestsDay3 {
 
     @Test
     public void test10(){
-        JsonPath jsonPath= given().accept("application/json").when().get("/employees").
+        JsonPath json = given().accept("application/json").when().get("/employees").
                 thenReturn().jsonPath();
-        String namoOfEmployee = jsonPath.getString("items[0].last_name");
+        String namoOfEmployee = json.getString("items[0].last_name");
         System.out.println(namoOfEmployee);
+
+        Map<String, ?> firstEmployee = json.get("items[0]"); // we put ? because it can be also not String
+        System.out.println(firstEmployee);
+
+        //since firstEmployee it's a map (key-value pair, we can iterate through it by using Entry. entry represent one key=value pair)
+        // put ? as a value (Map<String, ?>), because there are values of different data type: string, integer, etc..
+        //if you put String as value, you might get some casting exception that cannot convert from integer(or something else) to string
+        for (Map.Entry<String, ?> entry : firstEmployee.entrySet()) {
+            System.out.println("key: " + entry.getKey() + ", value: " + entry.getValue());
+        }
+
+//       get and print all last names
+//        items it's an object. whenever you need to read some property from the object, you put object.property
+//        but, if response has multiple objects, we can get property from every object
+        List<String> lastNames = json.get("items.last_name");
+        for (String str : lastNames) {
+            System.out.println("last name: " + str);
+        }
+
+
     }
+
+    @Test
+    public void test11(){
+        List<Integer> salaries = given().accept("application/json")
+                .when().get("/employees")
+                .thenReturn().jsonPath().get("items.salary");
+        Collections.sort(salaries);
+        Collections.reverse(salaries);
+        System.out.println(salaries);
+    }
+
+    @Test // replace each phone number . with -
+    public void test12(){
+        List<String> phoneNumbers =given().accept("application/json")
+                .when().get("/employees")
+                .thenReturn().jsonPath().get("items.phone_number");
+
+        phoneNumbers.replaceAll(phone -> phone.replace(".","-"));
+
+        System.out.println(phoneNumbers);
+    }
+
+    /**Task: Given accept type as Json
+     * And pathparams is id
+     * when user sends get request to locations
+     * then user verifies that status is 200
+     * and user verifies that location_id is 1700
+     * and user verifies that postal_code is 98199
+     * And user verifies that city Seattle
+     * And user verifies that state_province is Washington
+     */
+    @Test
+    public void test13(){
+        given().accept("application/json")
+                .pathParam("id", "1700")
+                .when().get("locations/{id}")
+                .then().assertThat().statusCode(200)
+                .and().assertThat().body("location_id",is(1700),
+                "postal_code",is("98199"),
+                "city",is("Seattle"),
+                "state_province",is("Washington") )
+                .log().all(true);
+    }
+    @Test
+    public void test14(){
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id", "1700")
+                .when().get("locations/{id}");
+
+        response.then().assertThat().statusCode(200);
+        response.then().assertThat().body("location_id",is(1700));
+        response.then().assertThat().body("postal_code",is("98199"));
+        response.then().assertThat().body("city",is("Seattle"));
+        response.then().assertThat().body("state_province",is("Washington"));
+        response.prettyPeek();
+
+    }
+
+
+
+
 
 
 
